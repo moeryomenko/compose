c3b8ee271d517615ddd49cb3d65c9bb573f5ccba
Maxim Eryomenko <moeryomenko@gmail.com>
2021-04-22 14:41:44 +0300

    [entrypoint-initdb] make possible create multidb
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

--- docker-entrypoint-initdb.d/prepare-database.sh
+++ docker-entrypoint-initdb.d/prepare-database.sh
@@ -1,9 +1,23 @@
+# Copy from https://github.com/mrts/docker-postgresql-multiple-databases
 #!/usr/bin/env bash
-set -euo pipefail
 
-# Run migrations.
-#
-# example:
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-  CREATE EXTENSION IF NOT EXISTS hstore;
+set -e
+set -u
+
+function create_user_and_database() {
+	local database=$1
+	echo "  Creating user and database '$database'"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+	    CREATE USER $database;
+	    CREATE DATABASE $database;
+	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
 EOSQL
+}
+
+if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
+	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
+	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
+		create_user_and_database $db
+	done
+	echo "Multiple databases created"
+fi
